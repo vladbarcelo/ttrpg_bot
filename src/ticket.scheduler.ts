cbcb9d3eb7fd859a1b0f4af57c5f7e07e396f249
Vlad Barcelo <vladbarcelo@mail.ru>
2025-07-02 12:56:56 +0300

    refactor(ticket): change notification routine

--- src/ticket.scheduler.ts
+++ src/ticket.scheduler.ts
@@ -6,136 +6,129 @@ import { TicketService } from './ticket.service';
 import { Telegraf } from 'telegraf';
 import { InjectBot } from 'nestjs-telegraf';
 import { TicketStatus } from '@prisma/client';
+import { CampaignService } from './campaign.service';
 
 @Injectable()
 export class TicketScheduler {
   constructor(
     private readonly prisma: PrismaService,
-    private readonly userService: UserService,
-    private readonly ticketService: TicketService,
+    private readonly campaignService: CampaignService,
     @InjectBot() private readonly bot: Telegraf<any>,
   ) {}
 
   @Cron(CronExpression.EVERY_5_MINUTES)
   async handleTicketDropsAndConfirmations() {
     const now = new Date();
-    const sessions = await this.prisma.session.findMany({
-      where: {
-        dateTime: {
-          gte: new Date(now.getTime() + 23 * 60 * 60 * 1000), // 23h from now
-          lte: new Date(now.getTime() + 49 * 60 * 60 * 1000), // 49h from now
+    const session = await this.campaignService.getNextSession();
+
+    const msToSession = session.dateTime.getTime() - now.getTime();
+    const hToSession = msToSession / (60 * 60 * 1000);
+
+    // priority drop
+    if (!session.priorityDropNotified) {
+      const priorityUsers = await this.prisma.user.findMany({
+        where: { isPriority: true },
+        include: {
+          tickets: true,
         },
-      },
-      include: { campaign: true, tickets: true },
-    });
-
-    for (const session of sessions) {
-      const msToSession = session.dateTime.getTime() - now.getTime();
-      const hToSession = msToSession / (60 * 60 * 1000);
-
-      // 48h before: priority drop
-      if (
-        hToSession > 47.9 &&
-        hToSession < 48.1 &&
-        !session.priorityDropNotified
-      ) {
-        const priorityUsers = await this.prisma.user.findMany({
-          where: { isPriority: true },
-        });
-        for (const user of priorityUsers) {
-          await this.bot.telegram.sendMessage(
-            user.telegramId,
-            `👑 Открыто приоритетное бронирование для сессии ${session.id} (${session.campaign.name})`,
-            {
-              reply_markup: {
-                keyboard: [[{ text: '/book' }]],
-              },
-            },
-          );
+      });
+      for (const user of priorityUsers) {
+        if (user.tickets.some((t) => t.sessionId === session.id)) {
+          continue;
         }
-        await this.prisma.session.update({
-          where: { id: session.id },
-          data: { priorityDropNotified: true },
-        });
-      }
 
-      // 25h before: ask for confirmation
-      if (
-        hToSession > 24.9 &&
-        hToSession < 25.1 &&
-        !session.confirmationRequested
-      ) {
-        const tickets = await this.prisma.ticket.findMany({
-          where: {
-            sessionId: session.id,
-            status: TicketStatus.BOOKED,
+        await this.bot.telegram.sendMessage(
+          user.telegramId,
+          `👑 Открыто приоритетное бронирование для сессии ${session.id} (${session.campaign.name})`,
+          {
+            reply_markup: {
+              keyboard: [[{ text: '/book' }]],
+            },
           },
-        });
-        for (const ticket of tickets) {
-          const user = await this.prisma.user.findUnique({
-            where: { id: ticket.userId },
-          });
-          if (user) {
-            await this.bot.telegram.sendMessage(
-              user.telegramId,
-              `🔥 Пожалуйста, подтвердите ваше бронирование для сессии ${session.id} (${session.campaign.name}) в течение 3 часов, иначе ваш билет будет отменен.`,
-              {
-                reply_markup: {
-                  keyboard: [[{ text: '/confirm' }]],
-                },
-              },
-            );
-          }
-        }
-        await this.prisma.session.update({
-          where: { id: session.id },
-          data: { confirmationRequested: true },
-        });
+        );
       }
+      await this.prisma.session.update({
+        where: { id: session.id },
+        data: { priorityDropNotified: true },
+      });
+    }
 
-      // 22h before: non-priority drop and unbook unconfirmed tickets
-      if (
-        hToSession > 21.9 &&
-        hToSession < 22.1 &&
-        !session.nonPriorityDropNotified
-      ) {
-        // Unbook unconfirmed tickets
-        const tickets = await this.prisma.ticket.findMany({
-          where: { sessionId: session.id, status: 'BOOKED' },
+    // 25h before: ask for confirmation
+    if (
+      hToSession > 24.9 &&
+      hToSession < 25.1 &&
+      !session.confirmationRequested
+    ) {
+      const tickets = await this.prisma.ticket.findMany({
+        where: {
+          sessionId: session.id,
+          status: TicketStatus.BOOKED,
+        },
+      });
+      for (const ticket of tickets) {
+        const user = await this.prisma.user.findUnique({
+          where: { id: ticket.userId },
         });
-        for (const ticket of tickets) {
-          await this.prisma.ticket.delete({
-            where: { id: ticket.id },
-          });
-          const user = await this.prisma.user.findUnique({
-            where: { id: ticket.userId },
-          });
-          if (user) {
-            await this.bot.telegram.sendMessage(
-              user.telegramId,
-              `‼️ Ваш билет для сессии ${session.id} (${session.campaign.name}) был отменен из-за не подтверждения бронирования.`,
-            );
-          }
-        }
-        // Notify all users about non-priority drop
-        const users = await this.prisma.user.findMany();
-        for (const user of users) {
+        if (user) {
           await this.bot.telegram.sendMessage(
             user.telegramId,
-            `⚡ Открыто бронирование для сессии ${session.id} (${session.campaign.name})`,
+            `🔥 Пожалуйста, подтвердите ваше бронирование для сессии ${session.id} (${session.campaign.name}) в течение 3 часов, иначе ваш билет будет отменен.`,
             {
               reply_markup: {
-                keyboard: [[{ text: '/book' }]],
+                keyboard: [[{ text: '/confirm' }]],
               },
-              disable_notification: true,
             },
           );
         }
-        await this.prisma.session.update({
-          where: { id: session.id },
-          data: { nonPriorityDropNotified: true },
-        });
       }
+      await this.prisma.session.update({
+        where: { id: session.id },
+        data: { confirmationRequested: true },
+      });
+    }
+
+    // 22h before: non-priority drop and unbook unconfirmed tickets
+    if (
+      hToSession > 21.9 &&
+      hToSession < 22.1 &&
+      !session.nonPriorityDropNotified
+    ) {
+      // Unbook unconfirmed tickets
+      const tickets = await this.prisma.ticket.findMany({
+        where: { sessionId: session.id, status: 'BOOKED' },
+      });
+      for (const ticket of tickets) {
+        await this.prisma.ticket.delete({
+          where: { id: ticket.id },
+        });
+        const user = await this.prisma.user.findUnique({
+          where: { id: ticket.userId },
+        });
+        if (user) {
+          await this.bot.telegram.sendMessage(
+            user.telegramId,
+            `‼️ Ваш билет для сессии ${session.id} (${session.campaign.name}) был отменен из-за не подтверждения бронирования.`,
+          );
+        }
+      }
+      // Notify all users about non-priority drop
+      const users = await this.prisma.user.findMany();
+      for (const user of users) {
+        await this.bot.telegram.sendMessage(
+          user.telegramId,
+          `⚡ Открыто бронирование для сессии ${session.id} (${session.campaign.name})`,
+          {
+            reply_markup: {
+              keyboard: [[{ text: '/book' }]],
+            },
+            disable_notification: true,
+          },
+        );
+      }
+      await this.prisma.session.update({
+        where: { id: session.id },
+        data: { nonPriorityDropNotified: true },
+      });
     }
   }
 }
